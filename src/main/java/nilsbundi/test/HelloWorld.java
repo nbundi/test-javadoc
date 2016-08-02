9624006df24b48ef6f5f6c2b4cab67b347374787
nbundi <nils.bundi@gmail.com>
2016-08-02 21:25:47 +0000

    initial commit

---
+++ src/main/java/nilsbundi/test/HelloWorld.java
@@ -0,0 +1,19 @@
+package nilsbundi.test;
+
+/**
+ * this simple class contains a single method printing 
+ * "Hello World Che" to the console
+ */
+public class HelloWorld {
+    
+    /**
+     * this method prints "Hello World Che" to the console
+     * 
+     * @param argvs 
+     * @return
+     */
+    public static void main(String[] argvs) {
+        String a = "Che";
+        System.out.println("Hello World " + a + "!");
+    }
+}
